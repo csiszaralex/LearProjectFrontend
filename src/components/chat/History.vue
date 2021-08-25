46d49810aa2edd9ec253a4da19ebf359c8b43b09
Alex <csiszaralex@gmail.com>
2021-08-26 01:31:54 +0200

    :sparkles: Adds histroy for chat

---
+++ src/components/chat/History.vue
@@ -0,0 +1,39 @@
+<template>
+  <router-link
+    v-for="item in items"
+    :key="item.id"
+    :to="`/chat/${item.id}`"
+    class="d-flex h6 p-2 py-3 borders text-decoration-none text-dark"
+  >
+    <fa-icon icon="user-circle" class="fa-2x mr-3" />
+    <div class="d-flex flex-column">
+      <div class="pb-1 font-weight-bold text-capitalize">{{ item.name }}</div>
+      <div class="pl-2">{{ item.type ? '' : 'Te:&nbsp;' }}{{ item.message }}</div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+import { ref } from 'vue';
+import axios from '../../config/axios';
+export default {
+  name: 'History',
+  setup() {
+    const items = ref();
+    function get() {
+      axios.get('chat/history').then(({ data }) => {
+        items.value = data;
+      });
+    }
+    get();
+
+    return { items };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.borders {
+  border-bottom: 0.1rem solid $dark;
+}
+</style>
